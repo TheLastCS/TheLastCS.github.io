264d077bc557df5b3368c079e1e2cff252f191f6
Christian <christianstewart5111@gmail.com>
2023-08-02 10:34:34 +0800

    Updated Resume

--- assets/js/resumemain.js
+++ assets/js/resumemain.js
@@ -103,7 +103,7 @@ let opt = {
     margin:       1,
     filename:     'StewartResume.pdf',
     image:        { type: 'png', quality: 0.98 },
-    html2canvas:  { scale: 2 },
+    html2canvas:  { scale: 1 },
     jsPDF:        { format: 'a4', orientation: 'portrait' }
 };
 
